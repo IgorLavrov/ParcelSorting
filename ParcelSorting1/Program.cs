549aac5318fc1d28bf56c2185f2d809de56ddeb4
Laptop <Laptop@DESKTOP-NEQI063>
2023-08-31 09:58:15 +0300

    Add project files.

---
+++ ParcelSorting1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ParcelSorting1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
